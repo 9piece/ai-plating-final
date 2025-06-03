7d3907a1eebdac359c8a1475e438c04902c7a68f
9piece <1209335192@qq.com>
2025-06-03 19:25:22 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer YOUR_API_KEY"
+        "Authorization": "Bearer 0bce1539-21ee-486d-acdd-95ecc559e984"
       },
       body: JSON.stringify({
         model: "ep-20250603185353-xr5tb",
